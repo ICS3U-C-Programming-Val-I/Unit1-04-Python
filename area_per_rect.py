15c38d8d8ade16558d269c1720c1c94c73a08443
Val Ijaola <valian.ijaola@stu.ocsb.ca>
2023-09-19 02:15:45 +0000

    Added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Created By: Val Ijaola
+# Date: September 18, 2023
+# This program Calculates the area and perimeter of a rectangle with the dimensions of 5cm x 6cm.
+
+
+def main():
+    print("If a rectangle has the dimensions:")
+    print("5cm x 6cm")
+    print("")
+    print("The area is {}cm^2".format(5 * 6))
+    print("The perimeter is {}cm".format(5 + 6 + 6 + 5))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Created By: Val Ijaola
+# Date: September 18, 2023
+# This program says hello world and is stylish.
+
+
+def main():
+    print(8.6 + 9.24)
+    print(9.25 - 3.9)
+    print(9.1 * 3.2)
+    print(9.2 / 2.3)
+    print(9.5**2)
+    print(16**0.5)
+
+
+if __name__ == "__main__":
+    main()
